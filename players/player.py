36060b62f7f4758cf88f772a576a1e83f1be2072
nutaro <nutaro@protonmail.com>
2021-02-04 13:18:04 -0300

    refactor

--- players/player.py
+++ players/player.py
@@ -1,28 +1,29 @@
 from players import Player
+from proprietys import Propriety
 
 
 class Player:
 
     def __init__(self, *args, **kwargs):
-        self.__money = 300
-        self.__owned_propriety = []
+        self._money = 300
+        self._owned_propriety = []
 
     @property
     def money(self):
-        return self.__money
+        return self._money
 
     @property
     def owned_propriety(self) -> list:
-        return self.__owned_propriety
+        return self._owned_propriety
 
     def pay_rent(self, value: int, owner: Player) -> None:
-        self.__money -= value
+        self._money -= value
         owner.receive_rent(value)
 
     def receive_rent(self, value: int) -> None:
-        self.__money += value
+        self._money += value
 
     def buy_property(self, propriety: Propriety):
-        self.__money -= propriety.cost
-        self.__owned_propriety.append(propriety)
+        self._money -= propriety.cost
+        self._owned_propriety.append(propriety)
         propriety.owner = self
