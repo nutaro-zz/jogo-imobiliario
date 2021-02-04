bac5522385c1376e346f17bd45cd209fbddee45b
nutaro <nutaro@protonmail.com>
2021-02-04 13:09:18 -0300

    make propriety has owner

--- players/player.py
+++ players/player.py
@@ -1,18 +1,19 @@
 from players import Player
 
+
 class Player:
 
     def __init__(self, *args, **kwargs):
         self.__money = 300
-        self.__owned_property = []
+        self.__owned_propriety = []
 
     @property
     def money(self):
         return self.__money
 
     @property
-    def owned_property(self) -> list:
-        return self.__owned_property
+    def owned_propriety(self) -> list:
+        return self.__owned_propriety
 
     def pay_rent(self, value: int, owner: Player) -> None:
         self.__money -= value
@@ -21,6 +22,7 @@ class Player:
     def receive_rent(self, value: int) -> None:
         self.__money += value
 
-    def buy_property(self, property: Property):
-        self.__money -= property.cost
-        self.__owned_property.append(property)
+    def buy_property(self, propriety: Propriety):
+        self.__money -= propriety.cost
+        self.__owned_propriety.append(propriety)
+        propriety.owner = self
