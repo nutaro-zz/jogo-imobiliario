4943546e5959c29d65c945d8548822e288fdab57
nutaro <nutaro@protonmail.com>
2021-02-04 13:45:05 -0300

    added propriety rules

--- proprietys/propriety.py
+++ proprietys/propriety.py
@@ -8,3 +8,23 @@ class Propriety:
         self.__rent = rent
         self.__owner = None
 
+    @property
+    def cost(self) -> int:
+        return self.__cost
+
+    @property
+    def rent(self) -> int:
+        return self.__rent
+
+    @property
+    def owner(self) -> Player:
+        return self.__owner
+
+    @owner.setter
+    def owner(self, value: Player) -> None:
+        if self.has_owner():
+            raise ValueError("Property has owner")
+        self.__owner = value
+
+    def has_owner(self) -> bool:
+        return self.__owner is not None
